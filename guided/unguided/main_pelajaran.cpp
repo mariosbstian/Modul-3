01835ee44440560d14e41d91bb9c43161d1162e3
mariosbstian <mariosebastianbrs657@gmail.com>
2025-10-16 04:19:20 +0700

    Delete /unguided directory

--- guided/unguided/main_pelajaran.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "pelajaran.h"
-using namespace std;
-
-int main() {
-    pelajaran pel = create_pelajaran("Struktur Data", "STRUKDAT0xx1");
-    tampil_pelajaran(pel);
-    return 0;
-}
